33febbafff36c6b281be5f9a7dd9049349a8fda1
Denys Borysiuk <denisborisiuk@gmail.com>
2023-06-16 14:53:22 +0300

    Updates

---
+++ assets/Home-088938d2.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-0cddc2a8.js";const r=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"Home"}),e.jsx("p",{children:"This is the home page."})]});export{r as default};
--- assets/Home-3d486321.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-8397fd4f.js";const r=()=>e.jsxs("div",{children:[e.jsx("h1",{children:"Home"}),e.jsx("p",{children:"This is the home page."})]});export{r as default};
